783e3442940b4f3b8e95ab926ae4ecf0594c806f
Zexi Li <zexi.li@aliyun.com>
2015-07-04 00:34:11 +0800

    lisp/init-ruby.el: use pry for inf-ruby

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -1,7 +1,7 @@
 (require-package 'robe)
 (require-package 'inf-ruby)
 
-; robe
+;; robe
 (autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
 (autoload 'ac-robe-setup "ac-robe" "auto-complete robe" nil nil)
 (add-hook 'ruby-mode-hook 'robe-mode)
@@ -11,5 +11,14 @@
              '("\\.\\(?:gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist
              '("\\(Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+
+(require 'inf-ruby)
+;;(define-key inf-ruby-minor-mode (kbd "C-c C-z") 'run-ruby)
+(when (executable-find "pry")
+  (add-to-list 'inf-ruby-implementations '("pry" . "pry"))
+  (setq inf-ruby-default-implementation "pry"))
+
+(when (require 'smartparens nil 'noerror)
+  (require 'smartparens-ruby))
 
 (provide 'init-ruby)
